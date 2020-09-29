3834319c0b8dfb15a406a78179c0df9b88ebd66e
卢兴超 <luxingchao@bimwinner.com>
2020-09-29 19:41:29 +0800

    add scss

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # generate-join-websites-css
 Created with CodeSandbox
+### 为了方便生成 join 网站 css, 使用scss 管理比较方便
+
+
+#### 需要修改文件位置
+```
+/src/scss/joinstyle.scss
+```
+
+#### 如何使用
+```
+修改后，可以F12查看源代码, 在header 部分有 生成 style 标签，里面就是生成的css
+```
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./styles.scss";
+// import "./styles.scss";
+import "./scss/joinstyle.scss";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>需要修改文件位置</h1>
+      <h2>/src/scss/joinstyle.scss</h2>
     </div>
   );
 }
